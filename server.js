a70fef089a0984c6e2e64d78166fa46e9c32c643
Abreto Fu <m@abreto.net>
2017-02-09 13:10:14 +0800

    using express generator

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var tinydocs = express();
 
+tinydocs.use(express.static('public'));
+
 tinydocs.get('/', function(req, res) {
     res.send('Hello Tinydocs.\n');
 });
 
 tinydocs.listen(2222, function () {
     console.log('Tinydocs listen on port 2222.');
-})
+});
